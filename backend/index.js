3562c1e61329a7fc25d11ddc244ab662f2cef899
abi26anamo <abianamo282@gmail.com>
2023-06-17 09:23:16 +0300

    added updates

--- backend/index.js
+++ backend/index.js
@@ -10,37 +10,34 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
-
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 
-
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({path: __dirname+'/.env'});
-
-
+dotenv.config({ path: path.join(__dirname, ".env") });
 
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-app.use(cors({
-   origin:["https://yene-hub-frontend.vercel.app"],
-   methods : ["POST","GET","PATCH","DELETE"],
-   credentials:true
-}));
 
+/* Enable CORS */
+app.use(
+  cors({
+    origin: "https://yene-hub-frontend.vercel.app",
+    methods: ["POST", "GET", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 
 /* FILE STORAGE */
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/assets");
@@ -59,26 +56,25 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
+
 const PORT = process.env.PORT || 3001;
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.json("hello");
-})
+});
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   });
-
 
 app.listen(PORT, () => {
   console.log(`Connected at port ${PORT}`);
 });
-
-
