3aeb115e9ffaa78f77e49a9a642e4a6bac01b4d3
abi26anamo <abianamo282@gmail.com>
2023-06-17 08:34:32 +0300

    added updates

--- backend/index.js
+++ backend/index.js
@@ -31,8 +31,13 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use(cors({
+   origin:["https://yene-hub-frontend.vercel.app"],
+   methods : ["POST","GET","PATCH","DELETE"],
+   credentials:true
+}));
+
 
 /* FILE STORAGE */
 
