c4055dcc9effe819e362ae74ae349d9dfa9e5a3d
abi26anamo <abianamo282@gmail.com>
2023-06-17 17:03:37 +0300

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -29,19 +29,13 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* Enable CORS */
-app.use(
-  cors({
-    origin: "https://yene-hub.vercel.app",
-    methods: ["POST", "GET", "PATCH", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-})
+  res.header("Access-Control-Allow-Origin", "https://yene-hub.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
